8c2b705dddd3f3511757275f7d61908df1f5ef96
wilder78 <173951485+wilder78@users.noreply.github.com>
2026-06-12 10:16:05 -0500

    Depuración del codigo para poder iniciar sesión y loguearse

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { googleLogin } from "../controllers/auth.controllers.js";
-
-const router = Router();
-
-router.post("/google", googleLogin);
-
-export default router;
